d6e050ed010c114054ce38e795d2232b5baf4a57
medmouine <mohamed.mouine.2@ulaval.ca>
2020-01-20 14:28:43 -0500

    add linter

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,21 @@
-var http = require('http');
-var port = process.env.PORT || 2345;
+const http = require('http');
+const port = process.env.PORT || 2345;
 
 // @ts-ignore
-module.exports = http.createServer(function (req, res) {
-    res.writeHead(404, {'Content-Type': 'text/plain'});
+module.exports = http
+    .createServer(function(req, res) {
+        res.writeHead(404, { 'Content-Type': 'text/plain' });
 
-    if (req.url.indexOf('/suggestions') === 0) {
-        res.end(JSON.stringify({
-            suggestions: []
-        }));
-    } else {
-        res.end();
-    }
-}).listen(port, '127.0.0.1');
+        if (req.url.indexOf('/suggestions') === 0) {
+            res.end(
+                JSON.stringify({
+                    suggestions: [],
+                }),
+            );
+        } else {
+            res.end();
+        }
+    })
+    .listen(port, '127.0.0.1');
 
 console.log('Server running at http://127.0.0.1:%d/suggestions', port);
